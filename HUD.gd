a91049f8797646cf4620fe7f73f6465b8306734e
Bonono63 <bonoria63@gmail.com>
2023-04-04 23:04:14 -0400

    uibsdfbuh

--- HUD.gd
+++
@@ -1,6 +0,0 @@
-extends Panel
-
-@export var camera : Node3D
-
-func _process(_delta):
-	$Label.text = str("x: ",camera.position.x, "\ny: ", camera.position.y, "\nz: ", camera.position.z, "\n", Engine.get_frames_per_second())
